59488c2e406d73f2c797b02116e5304cf8b65f04
FRANCISCO ALBERTO PAULINO VALERIO <1106084@est.intec.edu.do>
2023-06-29 20:24:29 -0400

    Se agrega el eliminar usuario, y una fraccion de codigo aun erronea para el token de restablecer contraseña

--- Controllers/Credenciales.cs
+++ Controllers/Credenciales.cs
@@ -4,6 +4,8 @@ using NuGet.Packaging.Signing;
 using Microsoft.Data.SqlClient;
 using System.Data;
 using NuGet.Protocol.Plugins;
+using System.Net.Mail;
+using System.Net;
 
 namespace ACIAapi.Controllers
 {
@@ -59,6 +61,35 @@ namespace ACIAapi.Controllers
             }
         }
 
+        [HttpGet("GetPasswordByUser")]
+        public IActionResult GetPasswordByUser(string usuario)
+        {
+            var aciadb = new ACIADB();
+            string connectionString = aciadb.getCs();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("spObtenerContraseñaPorUsuario", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Usuario", usuario);
+
+                string contraseña = string.Empty;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        contraseña = reader["Clave"].ToString();
+                    }
+                }
+
+                return Ok(contraseña);
+            }
+        }
+
         [HttpPost("InstertarCredencial")]
         public IActionResult InsertarUsuario(string usuario, string clave, int rolID, int Sesion = 1)
         {
@@ -88,6 +119,120 @@ namespace ACIAapi.Controllers
                     {
                         log.InsertarLog("InstertarCredencial", "Standard ERROR", Sesion);
                         return BadRequest("Error al insertar el registro.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest("Error: " + ex.Message);
+                }
+            }
+        }
+
+        [HttpGet("GetTokenResetPassword")]
+        public IActionResult GetTokenResetPassword(string usuario)
+        {
+            var aciadb = new ACIADB();
+            string connectionString = aciadb.getCs();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("spObtenerContraseñaPorUsuario", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Usuario", usuario);
+
+                string contraseña = string.Empty;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        contraseña = reader["Clave"].ToString();
+                    }
+                }
+
+                // Generar token aleatorio
+                string token = GenerateRandomToken();
+
+                // Enviar correo electrónico con el token
+                SendResetPasswordEmail(usuario, token);
+
+                return Ok(token);
+            }
+        }
+
+        private string GenerateRandomToken()
+        {
+            // Generar una cadena aleatoria para el token
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            var token = new string(Enumerable.Repeat(chars, 10)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return token;
+        }
+
+        private void SendResetPasswordEmail(string usuario, string token)
+        {
+            // Configurar el correo electrónico
+            string from = "bkeylog71@gmail.com";
+            string to = "franciscoapvst@gmail.com";
+            string subject = "Reset Password";
+            string body = "Dear {usuario},\n\n"
+                + "\nPlease use the following token to reset your password: {token}\n\n"
+                + "Best regards,\nYour Application";
+
+            // Configurar el cliente SMTP con conexión segura
+            using (var client = new SmtpClient("smtp.gmail.com", 465))
+            {
+                client.EnableSsl = false;
+                client.UseDefaultCredentials = true;
+                client.Credentials = new NetworkCredential(from, "xriwmdsqvmwsopnh");
+
+                // Enviar el correo electrónico
+                try
+                {
+                    client.Send(from, to, subject, body);
+                }
+                catch (SmtpException ex)
+                {
+                    // Manejar la excepción del cliente SMTP
+                    Console.WriteLine("Error sending email: " + ex.Message);
+                    throw;
+                }
+            }
+        }
+
+
+        [HttpPost("EliminarCredencial")]
+        public IActionResult EliminarUsuario(string usuario, string clave)
+        {
+            var aciadb = new ACIADB();
+            string connectionString = aciadb.getCs();
+            string consulta = "DELETE FROM Credencial_Usuario WHERE Usuario = @Usuario AND Clave = @Clave";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand(consulta, connection);
+                comando.Parameters.AddWithValue("@Usuario", usuario);
+                comando.Parameters.AddWithValue("@Clave", clave);
+
+                try
+                {
+                    connection.Open();
+                    var log = new LogRepository();
+                    int filasAfectadas = comando.ExecuteNonQuery();
+                    if (filasAfectadas > 0)
+                    {
+                        log.InsertarLog("EliminarCredencial", "0000");
+                        return Ok("Usuario eliminado exitosamente.");
+                    }
+                    else
+                    {
+                        log.InsertarLog("EliminarCredencial", "Standard ERROR");
+                        return BadRequest("No se encontró el usuario o la contraseña no coincide.");
                     }
                 }
                 catch (Exception ex)
